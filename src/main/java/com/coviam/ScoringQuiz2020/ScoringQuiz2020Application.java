c82e92a1f0f449486bd912135383611634519ab1
Mohit Shah <44272065+shahmohit9975@users.noreply.github.com>
2020-01-29 11:07:13 +0530

    first commit

---
+++ src/main/java/com/coviam/ScoringQuiz2020/ScoringQuiz2020Application.java
@@ -0,0 +1,13 @@
+package com.coviam.ScoringQuiz2020;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScoringQuiz2020Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScoringQuiz2020Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/coviam/ScoringQuiz2020/ScoringQuiz2020ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coviam.ScoringQuiz2020;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScoringQuiz2020ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
